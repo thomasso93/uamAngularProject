ee3125611e0ec656e845c1121479406da3d66505
thomasso93 <thomasso93@gmail.com>
2016-01-08 14:59:44 +0100

    added order list

--- server/scripts/script.js
+++ server/scripts/script.js
@@ -3,8 +3,16 @@ var pizzaApp = angular.module('pizzaApp', []);
     pizzaApp.controller('mainController', function($scope, $http) {
         
         $scope.getContact = function () {
-                $http.get('/contact').success(function (res) {
-                    $scope.contactRes = res;
-                });
-            };
+            $http.get('/contact').success(function (res) {
+                $scope.contactRes = res;
+            });
+        };
+        
+        $scope.getMenu = function () {
+            $http.get('/menu').success(function (res) {
+                $scope.menuRes = res;
+            });
+            
+        };
+         
     });
